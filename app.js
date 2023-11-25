fffba6c9d0cf4503a6d00e8a61687fdf41ccd8cc
Samorano2022 <117679139+Samorano2022@users.noreply.github.com>
2023-11-25 12:09:21 +0100

    new fixes

--- app.js
+++ app.js
@@ -4,32 +4,7 @@ var menu = document.getElementsByClassName('header__desktop');
 const hamburger = document.getElementsByClassName('hamburger')
 const header = document.getElementsByClassName('header__mobile')
 const header__wrapper = document.getElementsByClassName('header__wrapper')
-// const headerMain = document.getElementsByClassName('header')
 
-// function openTab(tabName){
-   
-
-//     for(btn of eventBtn){
-//         if(btn.id === tabName){
-//             btn.classList.add("active__button")
-//             console.log(true)
-//         }
-//         else{
-//             btn.classList.remove("active__button")
-//         }
-//     }
-//     for(table of eventTable){
-//         if(table.id === tabName){
-//             table.classList.add("active__table")
-//             console.log(true)
-//         }
-//         else{
-//             table.classList.remove("active__table")
-//         }
-//     }
-
-    
-//   }
 
   const openmenu =() => {
     menu[0].classList.toggle('active')
